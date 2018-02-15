9104e15353d097a324535888310f34a5205176d8
dustin deus <deusdustin@gmail.com>
2018-02-15 12:54:58 +0100

    implement confluence client and create space method

---
+++ commands/confluence/confluence_client.go
@@ -0,0 +1,81 @@
+package confluence
+
+import (
+	"io/ioutil"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+type (
+	// Client command to create git hooks
+	Client struct {
+		authMethod AuthMethod
+		client     *http.Client
+	}
+	// ClientOption function.
+	ClientOption func(*Client)
+	AuthMethod   interface {
+		auth(req *http.Request)
+	}
+)
+
+// NewClient with the given options.
+func NewClient(options ...ClientOption) *Client {
+	v := &Client{client: &http.Client{}}
+
+	for _, o := range options {
+		o(v)
+	}
+
+	return v
+}
+
+// WithAuth option.
+func WithAuth(auth AuthMethod) ClientOption {
+	return func(c *Client) {
+		c.authMethod = auth
+	}
+}
+
+func (c *Client) sendRequest(req *http.Request) ([]byte, error) {
+	req.Header.Add("Accept", "application/json, */*")
+	c.authMethod.auth(req)
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	switch resp.StatusCode {
+	case http.StatusOK, http.StatusCreated, http.StatusPartialContent:
+		res, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			return nil, err
+		}
+		return res, nil
+	case http.StatusNoContent, http.StatusResetContent:
+		return nil, nil
+	case http.StatusUnauthorized:
+		return nil, errors.Wrap(err, "Authentication failed.")
+	case http.StatusServiceUnavailable:
+		return nil, errors.Wrapf(err, "Service is not available (%s).", resp.Status)
+	case http.StatusInternalServerError:
+		return nil, errors.Wrapf(err, "Internal server error: %s", resp.Status)
+	}
+
+	return nil, errors.Wrapf(err, "Unknown response status %s", resp.Status)
+}
+
+type basicAuthCallback func() (username, password string)
+
+func (cb basicAuthCallback) auth(req *http.Request) {
+	username, password := cb()
+	req.SetBasicAuth(username, password)
+}
+
+// BasicAuth method
+func BasicAuth(username, password string) AuthMethod {
+	return basicAuthCallback(func() (string, string) { return username, password })
+}
